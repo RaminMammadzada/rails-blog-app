080310d244faca356ef911b9b3b362f73ef124e3
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-18 22:54:13 +0300

    Add login.

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
   resources :tags
   resources :authors
 
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
 end
