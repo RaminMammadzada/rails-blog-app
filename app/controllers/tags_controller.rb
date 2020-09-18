e3d7b34c88a1ef5e8fb1e963141150a01eb84f00
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-18 19:27:18 +0300

    Add tags to articles.

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,18 @@
+class TagsController < ApplicationController
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    flash.notice = "Tag '#{@tag.name}' Destroyed!"
+    redirect_to "/tags"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :tags
 
 end
